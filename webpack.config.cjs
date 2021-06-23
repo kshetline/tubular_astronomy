e27a15606c134a63809da13f478119f2c48d58d4
Kerry Shetline <kerry@shetline.com>
2021-06-23 01:10:13 -0400

    First release after improving fesm2015 generation.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,26 +2,18 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = env => {
-  const umd = !!env?.umd && (/^[ty]/i.test(env?.umd) || Number(env?.umd) !== 0);
-  const cjs = !umd && !!env?.cjs && (/^[ty]/i.test(env?.cjs) || Number(env?.cjs) !== 0);
-  const esVersion = umd ? 'es6' : 'es2018';
-  const dir = umd ? 'web' : (cjs ? 'cjs' : 'fesm2015');
-  const libraryTarget = umd ? 'umd' : (cjs ? 'commonjs' : 'module');
-  const asModule = !umd && !cjs;
-  const outFile = `index.${asModule ? 'm' : ''}js`;
+  const dev = !!env?.dev && (/^[ty]/i.test(env?.dev) || Number(env?.dev) !== 0);
+  const libraryTarget = 'umd';
 
-  const config = {
-    mode: env?.dev ? 'development' : 'production',
-    target: [esVersion, 'web'],
+  return {
+    mode: dev ? 'development' : 'production',
+    target: ['es6', 'web'],
     entry: './dist/index.js',
-    experiments: {
-      outputModule: asModule
-    },
     output: {
-      path: resolve(__dirname, 'dist/' + dir),
-      filename: outFile,
+      path: resolve(__dirname, 'dist', 'umd'),
+      filename: 'index.js',
       libraryTarget,
-      library: umd ? 'tbAstro' : undefined
+      library: 'tbAstro'
     },
     module: {
       rules: [
@@ -31,12 +23,12 @@ module.exports = env => {
             loader: 'babel-loader',
             options: {
               presets: [['@babel/preset-env', {
-                targets: { // min ES6 : min ES2018
-                  chrome:  umd ? '58' : '64',
-                  edge:    umd ? '14' : '79',
-                  firefox: umd ? '54' : '78',
-                  opera:   umd ? '55' : '51',
-                  safari:  umd ? '10' : '12',
+                targets: { // ES6 minimums
+                  chrome:  '58',
+                  edge:    '14',
+                  firefox: '54',
+                  opera:   '55',
+                  safari:  '10'
                 }
               }]]
             }
@@ -45,10 +37,9 @@ module.exports = env => {
         }
       ]
     },
-    externals: ['@tubular/time'],
-    externalsType: cjs ? 'commonjs' : 'global',
+    externals: [{ '@tubular/time': { commonjs: '@tubular/time', commonjs2: '@tubular/time', root: 'tbTime' } }],
     optimization: {
-      minimize: !env?.dev,
+      minimize: !dev,
       minimizer: [new TerserPlugin({
         terserOptions: {
           output: { max_line_len: 511 }
@@ -60,10 +51,4 @@ module.exports = env => {
       mainFields: ['fesm2015', 'module', 'main']
     }
   };
-
-  // Allow UMD target to bundle @tubular/array-buffer-reader, @tubular/math, and @tubular/util.
-  if (!umd)
-    config.externals.push(...['@tubular/array-buffer-reader', '@tubular/math', '@tubular/util']);
-
-  return config;
 };
